15f6f976b027204204928cff7495d3129b2f7b59
Vikas Gautam <v33vikasgautam@gmail.com>
2024-11-27 00:40:42 +0530

    fix: adds support for form-encoded requests

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ const io = new Server(server, {
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', require("./routes/user"))
 
